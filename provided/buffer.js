161d01f4e8758d0a423e9f7ff204de47f2fa0297
Eirik Vullum <evullum@gmail.com>
2017-07-19 13:29:38 +0200

    Add example buffers

--- provided/buffer.js
+++ provided/buffer.js
@@ -1,3 +1,50 @@
 /**
- * TODO
+ * Empty buffer
  */
+module.exports.none = function none() {
+  return {
+    put() {},
+    take() {},
+    isEmpty() {
+      return true;
+    },
+  }
+};
+
+/**
+ * Dropping buffer
+ */
+module.exports.dropping = function dropping(limit = 10) {
+  let buffer = [];
+
+  return {
+    put(msg) {
+      buffer = [...buffer, msg].slice(0, limit);
+    },
+    take() {
+      return buffer.shift();
+    },
+    isEmpty() {
+      return !!buffer.length;
+    },
+  }
+};
+
+/**
+ * Sliding buffer
+ */
+module.exports.sliding = function sliding(limit = 10) {
+  let buffer = [];
+
+  return {
+    put(msg) {
+      buffer = [...buffer, msg].slice().reverse().slice(0, limit).reverse()
+    },
+    take() {
+      return buffer.shift();
+    },
+    isEmpty() {
+      return !!buffer.length;
+    },
+  }
+};
